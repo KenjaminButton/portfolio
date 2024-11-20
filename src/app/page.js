f60ae69b7d0390c598e0d0799992aff5b237e1d0
Kenjamin Button <kennethpchang@gmail.com>
2024-11-19 21:03:06 -0800

    Background updated to central park NYC

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import bg from "../../public/background/home-background.png"
+import bg from "../../public/background/central-park.jpeg"
 
 export default function Home() {
   return (
