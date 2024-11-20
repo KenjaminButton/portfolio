df858b6078df673142900476807a5c2daca1f0e6
Kenjamin Button <kennethpchang@gmail.com>
2024-11-20 10:25:40 -0800

    Buttons paused when hover over

--- src/components/navigation/NavButton.jsx
+++ src/components/navigation/NavButton.jsx
@@ -61,8 +61,13 @@ const NavButton = ({x, y, label, link, icon, newTab}) => {
       className='absolute cursor-pointer z-50'
       style={{transform: `translate(${x}, ${y})`}}
     >
-      <Link href={link} target={newTab ? '_blank' : '_self'} className='text-foreground rounded-full flex items-center justify-center bg-background/20 border border-accent/30 border-solid backkdrop-blur-[6px] shadow-glass-inset hover:shadow-glass-sm' aria-label={label} name={label} >
-        <span className='relative w-14 h-14 p-4 animate-spin-slow-reverse'>
+      <Link 
+        href={link} 
+        target={newTab ? '_blank' : '_self'} 
+        className='text-foreground group rounded-full flex items-center justify-center bg-background/20 border border-accent/30 border-solid backkdrop-blur-[6px] shadow-glass-inset hover:shadow-glass-sm' 
+        aria-label={label} 
+        name={label} >
+        <span className='relative w-14 h-14 p-4 animate-spin-slow-reverse group-hover:text-accent group-hover:pause'>
           {getIcon(icon)}
         </span>
       </Link>
