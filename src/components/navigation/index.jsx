1f34b8a3ef8b950ee137923423a4f5f65a8c5c93
Kenjamin Button <kennethpchang@gmail.com>
2024-11-20 09:29:16 -0800

    Nav semi-circle rendered properly with calculations

--- src/components/navigation/index.jsx
+++ src/components/navigation/index.jsx
@@ -5,15 +5,26 @@ import React from 'react'
 
 const Navigation = () => {
 
-  
+  const angleIncrement = 360/BtnList.length
+
 
   return (
-    <div className='flex items-center justify-between relative'>
-      {
-        BtnList.map((btn, index) => {
-          return <button key={index}>{btn.label}</button>
-        })
-      }
+    <div className='fixed h-screen flex items-center justify-center'>
+      <div className='w-max flex items-center justify-between relative'>
+        {
+          BtnList.map((btn, index) => {
+            const angleRad = (index * angleIncrement * Math.PI) / 180
+            const radius = 'calc(20vw - 1rem)'
+            const x = `calc(${radius}*${Math.cos(angleRad)})`
+            const y = `calc(${radius}*${Math.sin(angleRad)})`
+
+            console.log(index, angleRad, radius, x, y)
+            return <button key={index} className='absolute' style={{
+              transform: `translate(${x}, ${y})`
+            }}>{btn.label}</button>
+          })
+        }
+      </div>
     </div>
   )
 }
